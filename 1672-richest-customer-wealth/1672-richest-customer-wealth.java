0cf819988d0fa2d1eba3fc54f6e1cb4fa2da5b9a
Yashraj Gadilkar <yashrajgadilkar191@gmail.com>
2026-08-20 22:43:45 +0530

    Time: 0 ms (100%), Space: 44.5 MB (56.32%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int ans=0;
+        for(int row=0;row<accounts.length;row++){
+            int sum=0;
+            for(int col=0;col<accounts[row].length;col++){
+                sum+=accounts[row][col];
+            }
+             if(ans < sum){
+               ans=sum;}
+        }
+        return ans;
+    }
+}
