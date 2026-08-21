a8f10b5af4368618c1a709839261f009e024363a
Yashraj Gadilkar <yashrajgadilkar191@gmail.com>
2026-08-21 08:06:51 +0530

    Time: 339 ms (58.08%), Space: 0B (100%) - LeetHub

--- 3673-find-zombie-sessions/3673-find-zombie-sessions.sql
+++ 3673-find-zombie-sessions/3673-find-zombie-sessions.sql
@@ -7,4 +7,4 @@ HAVING session_duration_minutes  > 30.0 AND scroll_count > 4
 AND (COUNT(CASE WHEN event_type = 'click' THEN 1 ELSE NULL END) / 
 CAST( scroll_count AS FLOAT) ) < 0.2
 AND COUNT(CASE WHEN event_type = 'purchase' THEN 1 ELSE NULL END) = 0
-ORDER BY scroll_count DESC, session_id
+ORDER BY scroll_count DESC, session_id;
